336369fda828e7f91536d58fad4d26761ea03c7e
immujahidkhan6@gmail.com <Password@2018>
2019-01-20 15:51:53 +0500

    Initial commit

---
+++ app/src/main/java/com/madeincanada/googlepaytestappsubscrition/Constants.java
@@ -0,0 +1,13 @@
+package com.madeincanada.googlepaytestappsubscrition;
+
+public class Constants {
+
+    public static final String SKU_DELAROY_MONTHLY = "IncomeTax_monthly";
+    public static final String SKU_DELAROY_THREEMONTH = "IncomeTax_threemonth";
+    public static final String SKU_DELAROY_SIXMONTH = "IncomeTax_sixmonth";
+    public static final String SKU_DELAROY_YEARLY = "IncomeTax_yearly";
+
+    public static final String base64EncodedPublicKey = "";
+
+
+}
